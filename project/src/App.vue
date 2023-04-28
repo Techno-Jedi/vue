1b09fffde9c10a71ed74496c1a5fb5e6ac6dbeff
Alex <aleksandr.leontev.n@mail.ru>
2023-04-28 18:51:54 +0300

    initial commit

---
+++ project/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="app">
+    <h1>Страница с постами</h1>
+    <my-button
+        @click="showDialog"
+        style="margin: 10px 0;"
+    >
+      Создать пост
+    </my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form
+          @create="createPost"
+      />
+    </my-dialog>
+    <post-list
+        :posts="posts"
+        @remove="removePost"
+  />
+  </div>
+</template>
+
+<script>
+import PostForm from "@/components/PostForm.vue";
+import PostList from "@/components/PostList.vue";
+
+export default {
+  components:{
+    PostForm, PostList
+  },
+  data() {
+    return{
+      posts:[
+      ],
+      dialogVisible:false,
+    }
+  },
+  methods: {
+    createPost(post){
+      this.posts.push(post);
+      this.dialogVisible = false;
+    },
+    removePost(post) {
+      this.posts = this.posts.filter(p => p.id !== post.id)
+    },
+    showDialog(){
+      this.dialogVisible = true;
+    }
+  }
+}
+
+</script>
+<style >
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
+}
+</style>
---
+++ project/src/components/PostForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <form @submit.prevent class="form">
+      <h2>Созданиие поста</h2>
+      <my-input
+          v-model="post.title"
+          type="text"
+          placeholder="Название"
+      />
+      <my-input
+          v-model="post.body"
+          type="text"
+          placeholder="Описание"
+      />
+      <my-button
+          @click="createPost"
+      >
+        Добавить пост</my-button>
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      post: {
+        title: '',
+        body: ''
+      }
+    }
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit('create', this.post)
+      this.post = {
+        title: '',
+        body: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.form{
+  display: flex;
+  flex-direction: column;
+}
+</style>
